16e568fc80cf06f3f2fbf8cb1466ee9e27f1389d
Blaž K <w4d4f4k@gmail.com>
2022-09-29 20:08:03 +0100

    still problems with memory and..
    fixing memory problems and code style..

--- testlagserv.sh
+++ testlagserv.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-#
+#--------------------------------------------------------------------------------------------
+# Useful to test memory problems on arduino/esp32 & similar.
+#--------------------------------------------------------------------------------------------
+# Usage: ./testlagserv.sh 50 0.1
+#        ./testlagserv.sh 50 1 http://192.168.4.1/upc=true
+#        ./testlagserv.sh 50
+#--------------------------------------------------------------------------------------------
+
 cnt=0
 max=$1
-testUrl=$2
+testUrl=$3
+delay=$2
 #
 if [[ $max == "" ]]; then
     max=3
@@ -10,6 +18,11 @@ fi
 if [[ $testUrl == "" ]]; then
     testUrl="http://192.168.4.1/"
 fi
+if [[ $delay == "" ]]; then
+    delay=1
+fi
+
+
 #
 echo "Starting testlagserv with max: "$max" and testUrl: "$testUrl
 #
@@ -17,7 +30,7 @@ while [ 1 ]; do
     echo $cnt".) testlagserv running..."
     #
     #curl -I $testUrl # Test HEAD only
-    #curl $testUrl    # Test with a lot of infos similar to browser
+    #curl $testUrl    # Test as browser
     # Test POST request with body
     curl -X POST http://192.168.4.1/up=true -H "Content-Type: application/json" -d '{"id":123,"name":"test"}'
     #curl -X POST http://192.168.4.1/reset -H "Content-Type: application/json" -d '{"id":123,"name":"test"}'
@@ -26,5 +39,5 @@ while [ 1 ]; do
     if [[ $cnt -ge $max ]]; then
         exit 1
     fi
-    sleep 1
+    sleep $delay
 done
